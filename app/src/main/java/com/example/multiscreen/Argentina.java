3c5ad6dbe938f71af0b535d1194aae6cc57097e4
russiankizune <moonlightcatg@gmail.com>
2024-03-23 10:17:39 +0900

    23.03.24

---
+++ app/src/main/java/com/example/multiscreen/Argentina.java
@@ -0,0 +1,14 @@
+package com.example.multiscreen;
+
+import androidx.annotation.Nullable;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Argentina extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.argentina);
+    }
+}
---
+++ app/src/main/java/com/example/multiscreen/Uruguay.java
@@ -0,0 +1,14 @@
+package com.example.multiscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Uruguay extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.uruguay);
+    }
+}
